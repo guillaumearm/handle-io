0865cb292fbcd27417a1c7930d1455f260178e7c
Guillaume ARM <trapcodien@gmail.com>
2018-02-17 20:32:29 +0100

    test(internal): utils#stringify

---
+++ src/internal/utils.spec.js
@@ -0,0 +1,10 @@
+const { stringify } = require('./utils')
+
+describe('internal/utils', () => {
+  describe('stringify', () => {
+    test('JSON.stringify with padding 2', () => {
+      const obj = { a: true, b: true, c: ['d', 'e', 'f'] };
+      expect(stringify(obj)).toMatchSnapshot();
+    });
+  });
+});
