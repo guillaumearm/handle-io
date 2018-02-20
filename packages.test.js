142b78c1a9e7a5cbe56cd4f4c1defa3d0735e9b2
Guillaume ARM <trapcodien@gmail.com>
2018-02-20 23:47:07 +0100

    test(packages): add test:packages script and packages.test.js (#17)

---
+++ packages.test.js
@@ -0,0 +1,30 @@
+const testHandleApi = (handleApi) => {
+  const { io, handler, testHandler } = handleApi;
+  expect(typeof io).toBe('function');
+  expect(typeof handler).toBe('function');
+  expect(typeof testHandler).toBe('function');
+  expect(Object.keys(handleApi)).toEqual(['io', 'handler', 'testHandler']);
+}
+
+describe('test bundles', () => {
+  describe('commonjs', () => {
+    test('should expose handle-io api', () => {
+      // eslint-disable-next-line node/no-missing-require
+      testHandleApi(require('./lib/handle-io.js'));
+    })
+  });
+
+  describe('es module', () => {
+    test('should expose handle-io api', () => {
+      // eslint-disable-next-line node/no-missing-require
+      testHandleApi(require('./es/handle-io.js'));
+    })
+  });
+
+  describe('umd module', () => {
+    test('should expose handle-io api', () => {
+      // eslint-disable-next-line node/no-missing-require
+      testHandleApi(require('./dist/handle-io.js'));
+    })
+  });
+});
