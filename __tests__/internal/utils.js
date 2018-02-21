ecf9aa45d67e5416ad948cf54dff59128a54bfc4
Guillaume ARM <trapcodien@gmail.com>
2018-02-21 20:53:07 +0100

    test(internal/utils): rename test description
    and update internal/utils snapshot

--- __tests__/internal/utils.js
+++ __tests__/internal/utils.js
@@ -1,6 +1,6 @@
 import { stringify } from '../../src/internal/utils';
 
-describe('internal/utils', () => {
+describe('handle-io/internal/utils', () => {
   describe('stringify', () => {
     test('JSON.stringify with padding 2', () => {
       const obj = { a: true, b: true, c: ['d', 'e', 'f'] };
