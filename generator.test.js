57f7b16bf73b9533708c909ac416ba08512a48c3
Guillaume ARM <trapcodien@gmail.com>
2018-02-21 01:17:12 +0100

    chore: clean repo for gh-pages

--- generator.test.js
+++
@@ -1,8 +0,0 @@
-describe('generator test', () => {
-  test('es6 generators functions should work', () => {
-    const gen = function*(){
-      return yield 42;
-    }
-    expect(gen().next()).toEqual({ value: 42, done: false });
-  })
-});
--- src/internal/utils.js
+++
@@ -1,1 +0,0 @@
-export const stringify = x => JSON.stringify(x, null, 2);
