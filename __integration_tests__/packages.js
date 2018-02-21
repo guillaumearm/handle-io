1f8581f74c5dcbe5ec09a807b7f83c8d85ffe7e5
Guillaume ARM <trapcodien@gmail.com>
2018-02-21 17:04:30 +0100

    test(packages): refactor testHandleIoApi

--- __integration_tests__/packages.js
+++ __integration_tests__/packages.js
@@ -1,30 +1,26 @@
 const testHandleIoApi = (handleApi) => {
-  const { io, handler, testHandler } = handleApi;
-  expect(typeof io).toBe('function');
-  expect(typeof handler).toBe('function');
-  expect(typeof testHandler).toBe('function');
-  expect(Object.keys(handleApi)).toEqual(['io', 'handler', 'testHandler']);
+  test('should expose handle-io api', () => {
+    const { io, handler, testHandler } = handleApi;
+    expect(typeof io).toBe('function');
+    expect(typeof handler).toBe('function');
+    expect(typeof testHandler).toBe('function');
+    expect(Object.keys(handleApi)).toEqual(['io', 'handler', 'testHandler']);
+  });
 }
 
 describe('packages', () => {
   describe('commonjs', () => {
-    test('should expose handle-io api', () => {
-      // eslint-disable-next-line node/no-missing-require
-      testHandleIoApi(require('../lib/handle-io.js'));
-    })
+    // eslint-disable-next-line node/no-missing-require
+    testHandleIoApi(require('../lib/handle-io.js'));
   });
 
   describe('es module', () => {
-    test('should expose handle-io api', () => {
-      // eslint-disable-next-line node/no-missing-require
-      testHandleIoApi(require('../es/handle-io.js'));
-    })
+    // eslint-disable-next-line node/no-missing-require
+    testHandleIoApi(require('../es/handle-io.js'));
   });
 
   describe('umd module', () => {
-    test('should expose handle-io api', () => {
-      // eslint-disable-next-line node/no-missing-require
-      testHandleIoApi(require('../dist/handle-io.js'));
-    })
+    // eslint-disable-next-line node/no-missing-require
+    testHandleIoApi(require('../dist/handle-io.js'));
   });
 });
