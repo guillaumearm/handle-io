149b6e88cb3c37848dd96bcc36be8db5d9c21bea
Guillaume ARM <trapcodien@gmail.com>
2018-02-21 17:06:58 +0100

    Refactor integration tests (#28)
    * test(packages): rename testHandleApi in testHandleIoApi
    
    in __integration_tests__/packages.js
    
    * test(packages): refactor testHandleIoApi

--- __integration_tests__/packages.js
+++ __integration_tests__/packages.js
@@ -1,30 +1,26 @@
-const testHandleApi = (handleApi) => {
-  const { io, handler, testHandler } = handleApi;
-  expect(typeof io).toBe('function');
-  expect(typeof handler).toBe('function');
-  expect(typeof testHandler).toBe('function');
-  expect(Object.keys(handleApi)).toEqual(['io', 'handler', 'testHandler']);
+const testHandleIoApi = (handleApi) => {
+  test('should expose handle-io api', () => {
+    const { io, handler, testHandler } = handleApi;
+    expect(typeof io).toBe('function');
+    expect(typeof handler).toBe('function');
+    expect(typeof testHandler).toBe('function');
+    expect(Object.keys(handleApi)).toEqual(['io', 'handler', 'testHandler']);
+  });
 }
 
 describe('packages', () => {
   describe('commonjs', () => {
-    test('should expose handle-io api', () => {
-      // eslint-disable-next-line node/no-missing-require
-      testHandleApi(require('../lib/handle-io.js'));
-    })
+    // eslint-disable-next-line node/no-missing-require
+    testHandleIoApi(require('../lib/handle-io.js'));
   });
 
   describe('es module', () => {
-    test('should expose handle-io api', () => {
-      // eslint-disable-next-line node/no-missing-require
-      testHandleApi(require('../es/handle-io.js'));
-    })
+    // eslint-disable-next-line node/no-missing-require
+    testHandleIoApi(require('../es/handle-io.js'));
   });
 
   describe('umd module', () => {
-    test('should expose handle-io api', () => {
-      // eslint-disable-next-line node/no-missing-require
-      testHandleApi(require('../dist/handle-io.js'));
-    })
+    // eslint-disable-next-line node/no-missing-require
+    testHandleIoApi(require('../dist/handle-io.js'));
   });
 });
