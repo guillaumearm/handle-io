8d9480455757fa3be428533655f22fb66e77795f
Guillaume ARM <trapcodien@gmail.com>
2018-02-22 16:04:10 +0100

    test(api): replace acutal tests by a jest snapshot
    Ref #21

--- __integration_tests__/api.js
+++ __integration_tests__/api.js
@@ -1,9 +1,5 @@
 module.exports = (handleIo) => {
-  test('should expose handle-io api', () => {
-    const { io, handler, testHandler } = handleIo;
-    expect(typeof io).toBe('function');
-    expect(typeof handler).toBe('function');
-    expect(typeof testHandler).toBe('function');
-    expect(Object.keys(handleIo)).toEqual(['io', 'handler', 'testHandler']);
+  test('should expose valid handle-io api', () => {
+    expect(handleIo).toMatchSnapshot();
   });
 }
