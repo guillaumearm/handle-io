d1f13e2595e54faaeaa17fde26cb490b9c616a4a
Guillaume ARM <trapcodien@gmail.com>
2018-03-11 18:04:44 +0100

    chore: add yolo in src/index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import io from './io';
 import handler from './handler';
 import testHandler from './testHandler';
 
+const yolo = () => true;
+
 export {
   io,
   handler,
   testHandler,
+  yolo,
 }
