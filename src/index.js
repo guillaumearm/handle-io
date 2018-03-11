6083ef66a0913129890940fd243a999d702f9e59
Guillaume ARM <trapcodien@gmail.com>
2018-03-11 18:25:46 +0100

    chore: repair coverage

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ import io from './io';
 import handler from './handler';
 import testHandler from './testHandler';
 
-// eslint-disable-next-line
-function yolo() {
-  void true;
-  return 42;
-}
-
 export {
   io,
   handler,
